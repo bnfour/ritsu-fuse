18af201b4dced32a80ee02a1907e7a0e5c47b11c
Bn4 <bnfour@bnfour.net>
2024-09-12 17:13:26 +0700

    consoleapp: parsing of all the arguments, model binding

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,15 +15,39 @@ public class Program
 
         var customVersionOption = new Option<bool>("--version", "Display versions for this app and used library");
         customVersionOption.AddAlias("-v");
-
         rootCommand.AddGlobalOption(customVersionOption);
-        rootCommand.SetHandler(boolean =>
+
+        var targetFolderArgument = new Argument<FileInfo>("target folder", "Folder with files to create a random symlink to. Must contain at least 2 files.");
+        var rootFolderArgument = new Argument<FileInfo>("file system root folder", "Folder to host the file system. Must exist and be empty.");
+
+        var timeoutOption = new Option<uint>("--timeout", () => 100, "Time (in milliseconds) between requests to continue returning the same target. Most apps read the link more than once.");
+
+        var verboseOption = new Option<bool>("--verbose", "Display diagnostic messages.");
+
+        var noRepeatsOption = new Option<bool>("--no-repeats", "Prevents the same file being targeted twice in a row.");
+        var queueOption = new Option<bool>("--queue", "Use shuffled queue instead of full random. Returns each file in random order once before repeating.");
+
+        var linkNameOption = new Option<string>("--link-name", () => "ritsu", "Name of the symlink in the file system folder.");
+
+        rootCommand.AddArgument(targetFolderArgument);
+        rootCommand.AddArgument(rootFolderArgument);
+        rootCommand.AddOption(timeoutOption);
+        rootCommand.AddOption(verboseOption);
+        rootCommand.AddOption(noRepeatsOption);
+        rootCommand.AddOption(queueOption);
+        rootCommand.AddOption(linkNameOption);
+
+        rootCommand.SetHandler((isVersion, settings) =>
         {
-            if (boolean)
+            if (isVersion)
             {
                 Console.WriteLine($"Console app {GetVersion().ToString(3)}, library {RitsuFuseWrapper.GetVersion().ToString(3)}");
             }
-        }, customVersionOption);
+            else
+            {
+                // TODO launch the app
+            }
+        }, customVersionOption, new SettingsBinder(targetFolderArgument, rootFolderArgument, timeoutOption, verboseOption, noRepeatsOption, queueOption, linkNameOption));
 
         // the default handler for --version takes over custom stuff,
         // so it is excluded from the pipeline here
